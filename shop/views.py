55c2d37af52a2eafd7ad3fc826ef8aed948b2b94
Stas <staskovalev111976@gmail.com>
2026-05-20 14:38:43 +0300

    3 лаба по сайту(№18)

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
+from .models import Product, Category, Manufacturer, Cart, CartItem
 
 def home(request):
     return HttpResponse("""
@@ -27,3 +32,87 @@ def store(request):
     <p>- Термос для кофе</p>
     <p>- Термокружка</p>
     """)
+
+# Список товаров
+def product_list(request):
+
+    products = Product.objects.all()
+
+    search = request.GET.get('search')
+
+    if search:
+        products = products.filter(
+            Q(name__icontains=search) |
+            Q(description__icontains=search)
+        )
+
+    return render(request, 'shop/product_list.html', {
+        'products': products
+    })
+
+# Детали товара
+def product_detail(request, pk):
+
+    product = get_object_or_404(Product, pk=pk)
+
+    return render(request, 'shop/product_detail.html', {
+        'product': product
+    })
+
+# Корзина
+@login_required
+def cart_view(request):
+
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    items = cart.items.all()
+
+    total = cart.total_price()
+
+    return render(request, 'shop/cart.html', {
+        'items': items,
+        'total': total
+    })
+
+# Добавить в корзину
+@login_required
+def add_to_cart(request, product_id):
+
+    product = get_object_or_404(Product, id=product_id)
+
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    item, created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product
+    )
+
+    if not created:
+        item.quantity += 1
+        item.save()
+
+    return redirect('cart')
+
+# Обновить корзину
+@login_required
+def update_cart(request, item_id):
+
+    item = get_object_or_404(CartItem, id=item_id)
+
+    quantity = int(request.POST.get('quantity'))
+
+    if quantity <= item.product.quantity:
+        item.quantity = quantity
+        item.save()
+
+    return redirect('cart')
+
+# Удалить из корзины
+@login_required
+def remove_from_cart(request, item_id):
+
+    item = get_object_or_404(CartItem, id=item_id)
+
+    item.delete()
+
+    return redirect('cart')
